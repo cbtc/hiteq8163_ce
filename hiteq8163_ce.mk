abe2af3253a089fe6a0a0a8fdee4569a28d7c5f6
cbtc <carlos_ddg@yahoo.es>
2018-04-21 23:02:28 +0200

    more changes partitions

--- hiteq8163_ce.mk
+++ hiteq8163_ce.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/CUBE/hiteq8163_ce/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hiteq8163_ce
-PRODUCT_NAME := omni_hiteq8163_ce
+PRODUCT_NAME := hiteq8163_ce
 PRODUCT_BRAND := CUBE
 PRODUCT_MODEL := U78
 PRODUCT_MANUFACTURER := CUBE
